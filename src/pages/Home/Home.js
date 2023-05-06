8a44d3515a9379c4814c8c07487d17e101cb9e98
Sarahsegla <sarahsegla@icloud.com>
2023-05-07 00:47:44 +0100

    added more to home about

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
 
+
 function Home () {
     return (
         <div>
@@ -14,15 +15,18 @@ function Home () {
           
           <div className="header">
                 <h1 className="name" style={{  fontFamily: 'Bebas Neue', float: 'left', padding: '10px', paddingRight: '20px' }}>Sarah Segla</h1>
-                <h2 style={{  fontFamily: 'Bebas Neue', float: 'left', padding: '10px', paddingRight: '20px' }}>Front-End Web Developer</h2>
+                <h2 style={{  fontFamily: 'Bebas Neue', float: 'left', paddingLeft: '10px', paddingRight: '20px' }}>Front-End Web Developer</h2>
             </div>
             <article>
-                <div className="about" style={{float: 'left'}}>
-                Hi, welcome to my portfolio page. I'm Web developer that is committed and has a positive work attitude. Experienced working alone and  in group collaborations.
-         Dedicated to improving my skills and learning more to  deliver feature rich websites.</div>
+                <h3 className="about" style={{float: 'left', paddingLeft: '10px', fontFamily: 'Comic Neue'}}>
+                Hi, welcome to my portfolio page. I'm web developer that is committed and has a positive work attitude. Experienced working alone and  in group collaborations.
+         Dedicated to improving my skills and learning more to  deliver feature rich websites. I have experience working with HTML5, CSS3, JavaScript,
+         jQuery, React.js Node and application programming interfaces.
+         </h3>
             </article>
-
-          
+            <div className='MoreInfo' style={{float: 'left', paddingLeft: '10px', fontFamily: 'Bebas Neue'}}><h4>Experience: 1+</h4>
+                <h4>Location: South Yorkshire, UK</h4>
+            </div>
         </Grid>
         <Grid item xs={8}>
         
