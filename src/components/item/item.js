9e61b061d659eeab17ee7c0837e07ba586979e0e
Sarahsegla <sarahsegla@icloud.com>
2023-05-06 23:41:25 +0100

    home page in grid

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -7,16 +7,17 @@ function Item(props)
     return (
         <Paper>
             <div className="mySlides" style={{textAlign: 'center'}}>
-            <img src={props.item.image} alt={props.item.description} style={{width: '700', height: '500', paddingTop: '10px' }}/>
+            <img src={props.item.image} alt={props.item.description} style={{width: '600', height: '500', paddingTop: '10px' }}/>
            
             </div>
-
+            <div className="buttons">
             <Button variant="text" className="LinkButton">
-            <a href={props.item.url} style={{ textDecoration: 'none', color: 'black', fontFamily: 'Bebas Neue', paddingLeft: '30px', fontSize: '40px' }} >{props.item.description}</a>
+            <a href={props.item.url}  style={{ textDecoration: 'none', color: 'black', fontFamily: 'Bebas Neue', paddingLeft: '30px', fontSize: '40px' }} >{props.item.description}</a>
             </Button>
             <Button variant="text" className="GitButton">
             <a href={props.item.GitUrl} style={{ textDecoration: 'none', color: 'black', fontFamily: 'Bebas Neue', paddingLeft: '1000px', fontSize: '40px' }}>GitHub Repository </a>
             </Button>
+            </div>
         </Paper>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,41 @@
 import React from "react";
 import "./styles.css";
 import Me from "../../image/me.jpg";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 
 function Home () {
     return (
         <div>
-            <div className="header">
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2} columns={16}>
+        <Grid item xs={8}>
+          
+          <div className="header">
                 <h1 className="name" style={{  fontFamily: 'Bebas Neue', float: 'left', padding: '10px', paddingRight: '20px' }}>Sarah Segla</h1>
+                <h2 style={{  fontFamily: 'Bebas Neue', float: 'left', padding: '10px', paddingRight: '20px' }}>Front-End Web Developer</h2>
             </div>
-           <img src={Me} alt="me" style={{paddingLeft: '300px', paddingTop: '20px'}}/>
-
-            
             <article>
                 <div className="about" style={{float: 'left'}}>
                 Hi, welcome to my portfolio page. I'm Web developer that is committed and has a positive work attitude. Experienced working alone and  in group collaborations.
          Dedicated to improving my skills and learning more to  deliver feature rich websites.</div>
             </article>
+
+          
+        </Grid>
+        <Grid item xs={8}>
+        
+          <img src={Me} alt="me" style={{paddingLeft: '30px', paddingTop: '20px'}}/>
+          
+        </Grid>
+      </Grid>
+            
            
+
+            
+            
+           </Box>
         </div>
     )
 }
